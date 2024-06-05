017951206f10c16ef79a71eb49a4267c2dab100c
Heet Patel <Heet Patel@WINDOWS-43PV03O>
2024-06-05 15:08:10 -0400

    Add project files.

---
+++ Copy of the BrawlDefence/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Copy_of_the_BrawlDefence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.insaneButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.livesLabel = new System.Windows.Forms.Label();
+            this.moneyLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.dragEdgar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.Location = new System.Drawing.Point(241, 249);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(75, 23);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "Easy";
+            this.easyButton.UseVisualStyleBackColor = true;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click_1);
+            this.easyButton.MouseLeave += new System.EventHandler(this.easyButton_MouseLeave);
+            this.easyButton.MouseHover += new System.EventHandler(this.easyButton_MouseHover);
+            // 
+            // hardButton
+            // 
+            this.hardButton.Location = new System.Drawing.Point(479, 249);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(75, 23);
+            this.hardButton.TabIndex = 1;
+            this.hardButton.Text = "Hard";
+            this.hardButton.UseVisualStyleBackColor = true;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click_1);
+            this.hardButton.MouseLeave += new System.EventHandler(this.hardButton_MouseLeave);
+            this.hardButton.MouseHover += new System.EventHandler(this.hardButton_MouseHover);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.Location = new System.Drawing.Point(363, 249);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(75, 23);
+            this.mediumButton.TabIndex = 2;
+            this.mediumButton.Text = "Medium";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButton_Click_1);
+            this.mediumButton.MouseLeave += new System.EventHandler(this.mediumButton_MouseLeave);
+            this.mediumButton.MouseHover += new System.EventHandler(this.mediumButton_MouseHover);
+            // 
+            // insaneButton
+            // 
+            this.insaneButton.Location = new System.Drawing.Point(363, 349);
+            this.insaneButton.Name = "insaneButton";
+            this.insaneButton.Size = new System.Drawing.Size(75, 23);
+            this.insaneButton.TabIndex = 3;
+            this.insaneButton.Text = "Insane";
+            this.insaneButton.UseVisualStyleBackColor = true;
+            this.insaneButton.Click += new System.EventHandler(this.insaneButton_Click_1);
+            this.insaneButton.MouseLeave += new System.EventHandler(this.insaneButton_MouseLeave);
+            this.insaneButton.MouseHover += new System.EventHandler(this.insaneButton_MouseHover);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(653, 35);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.exitButton.MouseLeave += new System.EventHandler(this.exitButton_MouseLeave);
+            this.exitButton.MouseHover += new System.EventHandler(this.exitButton_MouseHover);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(396, 146);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(0, 13);
+            this.titleLabel.TabIndex = 5;
+            // 
+            // livesLabel
+            // 
+            this.livesLabel.AutoSize = true;
+            this.livesLabel.Location = new System.Drawing.Point(126, 19);
+            this.livesLabel.Name = "livesLabel";
+            this.livesLabel.Size = new System.Drawing.Size(32, 13);
+            this.livesLabel.TabIndex = 6;
+            this.livesLabel.Text = "Lives";
+            // 
+            // moneyLabel
+            // 
+            this.moneyLabel.AutoSize = true;
+            this.moneyLabel.Location = new System.Drawing.Point(37, 19);
+            this.moneyLabel.Name = "moneyLabel";
+            this.moneyLabel.Size = new System.Drawing.Size(39, 13);
+            this.moneyLabel.TabIndex = 7;
+            this.moneyLabel.Text = "Money";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick_1);
+            // 
+            // dragEdgar
+            // 
+            this.dragEdgar.BackColor = System.Drawing.Color.Transparent;
+            this.dragEdgar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dragEdgar.Location = new System.Drawing.Point(12, 108);
+            this.dragEdgar.Name = "dragEdgar";
+            this.dragEdgar.Size = new System.Drawing.Size(64, 51);
+            this.dragEdgar.TabIndex = 8;
+            this.dragEdgar.Text = "Drag button";
+            this.dragEdgar.UseVisualStyleBackColor = false;
+            this.dragEdgar.DragOver += new System.Windows.Forms.DragEventHandler(this.dragEdgar_DragOver);
+            this.dragEdgar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragEdgar_MouseDown);
+            this.dragEdgar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dragEdgar_MouseMove);
+            this.dragEdgar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dragEdgar_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.dragEdgar);
+            this.Controls.Add(this.moneyLabel);
+            this.Controls.Add(this.livesLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.insaneButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.easyButton);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button insaneButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label livesLabel;
+        private System.Windows.Forms.Label moneyLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button dragEdgar;
+    }
+}
+
---
+++ Copy of the BrawlDefence/Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Copy_of_the_BrawlDefence
+{
+    public partial class Form1 : Form
+    {
+        Rectangle firstTurn = new Rectangle(675, 195, 30, 30);
+        Rectangle secondTurn = new Rectangle(500, 170, 30, 30);
+        Rectangle thirdTurn = new Rectangle(525, 75, 30, 30);
+        Rectangle fourthTurn = new Rectangle(145, 100, 410, 30);
+
+        Point edgaroffset;
+
+        int screen = 0;
+        int time = 600;
+        int loadingBar;
+
+        int lives;
+        int money;
+
+        bool isDragging = false;
+
+        Pen blackPen = new Pen(Color.Black, 3);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush brownBrush = new SolidBrush(Color.Peru);
+
+        List<int> robots = new List<int>();
+        List<string> robotsColour = new List<string>();
+        List<int> robotsSize = new List<int>();
+        List<int> robotsSpeed = new List<int>();
+
+        Image edgarLogo = (Properties.Resources.edgar_logo);
+        Image crowLogo = (Properties.Resources.crow_logo);
+        Image jackyLogo = (Properties.Resources.jacky_logo);
+        Image pocoLogo = (Properties.Resources.poco_logo);
+        Image nitaLogo = (Properties.Resources.nita_logo);
+
+        Rectangle edgarSquare = new Rectangle(23, 90, 50, 50);
+        Rectangle crowSquare = new Rectangle(23, 150, 50, 50);
+        Rectangle jackySquare = new Rectangle(23, 220, 50, 50);
+        Rectangle pocoSquare = new Rectangle(23, 280, 50, 50);
+        Rectangle nitaSquare = new Rectangle(23, 340, 50, 50);
+
+        List<Rectangle> edgars = new List<Rectangle>();
+
+        Random randGen = new Random();
+
+        int randValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void menuTimer_Tick(object sender, EventArgs e)
+        {
+            //Show Menu Screen
+
+            //Check to see if easy button has been pressed
+
+            //Check to see if medium button has been pressed
+
+            //Check to see if hard button has been pressed
+
+            //Check to see if insane button has been pressed
+
+            //Check to see if exit button has been pressed
+
+            Refresh();
+        }
+        public void towerGrabbed()
+        {
+
+        }
+        public void towerUpgraded()
+        {
+
+        }
+        public void towerSold()
+        {
+
+        }
+        public void shootBullet()
+        {
+
+        }
+        public void moveBullet()
+        {
+
+        }
+        public void removeBullet()
+        {
+
+        }
+        public void bulletHitRobot()
+        {
+
+        }
+        public void robotKill()
+        {
+
+        }
+        public void loseLives()
+        {
+
+        }
+        public void outOfLives()
+        {
+
+        }
+        public void waveCompleted()
+        {
+
+        }
+        public void gameCompleted()
+        {
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+            //Make animation
+            if (screen == 0)
+            {
+                if (time > 0 && loadingBar < 300)
+                {
+                    this.BackColor = (Color.White);
+                    e.Graphics.FillRectangle(redBrush, 200, 300, loadingBar, 50);
+                    e.Graphics.DrawRectangle(blackPen, 200, 300, 375, 50);
+
+                    loadingBar = loadingBar + 40;
+                    time--;
+                }
+                else if (time > 0 && loadingBar < 353)
+                {
+                    this.BackColor = (Color.White);
+                    e.Graphics.FillRectangle(redBrush, 200, 300, loadingBar, 50);
+                    e.Graphics.DrawRectangle(blackPen, 200, 300, 375, 50);
+
+                    randValue = randGen.Next(0, 101);
+
+                    if (randValue > 95)
+                    {
+                        loadingBar = loadingBar + 40;
+                    }
+                    time--;
+                }
+                else if (time > 0 && loadingBar < 375)
+                {
+                    this.BackColor = (Color.White);
+                    e.Graphics.FillRectangle(redBrush, 200, 300, loadingBar, 50);
+                    e.Graphics.DrawRectangle(blackPen, 200, 300, 375, 50);
+
+                    loadingBar = loadingBar + 10;
+                    time--;
+                    screen = 1;
+                }
+                else
+                {
+                    Thread.Sleep(500);
+                    e.Graphics.Clear(Color.White);
+                }
+                easyButton.Visible = false;
+                mediumButton.Visible = false;
+                hardButton.Visible = false;
+                insaneButton.Visible = false;
+                exitButton.Visible = false;
+                titleLabel.Visible = false;
+                moneyLabel.Visible = false;
+                livesLabel.Visible = false;
+            }
+            else if (screen == 1)
+            {
+               this.BackColor = Color.White;
+                easyButton.Visible = true;
+                mediumButton.Visible = true;
+                hardButton.Visible = true;
+                insaneButton.Visible = true;
+                exitButton.Visible = true;
+                titleLabel.Visible = true;
+                moneyLabel.Visible= true;
+                livesLabel.Visible = true;
+            }
+            else if (screen == 2)
+            {
+                this.BackColor = Color.White;
+                easyButton.Visible = false;
+                mediumButton.Visible = false;
+                hardButton.Visible = false;
+                insaneButton.Visible = false;
+                exitButton.Visible = false;
+                titleLabel.Visible = false;
+                livesLabel.Visible = true;
+                moneyLabel.Visible = true;
+
+                livesLabel.Text = $"Lives : {lives}";
+                moneyLabel.Text = $"Money : {money}";
+
+                //Draw Path
+                e.Graphics.FillRectangle(brownBrush, 675, 0, 30, 170);
+                e.Graphics.FillRectangle(brownBrush, 525, 170, 180, 30);
+                e.Graphics.FillRectangle(brownBrush, 525, 100, 30, 70);
+                e.Graphics.FillRectangle(brownBrush, 145, 100, 410, 30);
+                e.Graphics.FillRectangle(brownBrush, 145, 100, 30, 180);
+                e.Graphics.FillRectangle(brownBrush, 145, 250, 80, 30);
+                e.Graphics.FillRectangle(brownBrush, 225, 180, 30, 100);
+                e.Graphics.FillRectangle(brownBrush, 225, 180, 100, 30);
+                e.Graphics.FillRectangle(brownBrush, 325, 180, 30, 145);
+                e.Graphics.FillRectangle(brownBrush, 355, 295, 200, 30);
+                e.Graphics.FillRectangle(brownBrush, 525, 245, 30, 50);
+                e.Graphics.FillRectangle(brownBrush, 555, 245, 180, 30);
+                e.Graphics.FillRectangle(brownBrush, 715, 245, 30, 135);
+                e.Graphics.FillRectangle(brownBrush, 250, 380, 495, 30);
+                e.Graphics.FillRectangle(brownBrush, 250, 320, 30, 60);
+                e.Graphics.FillRectangle(brownBrush, 145, 320, 105, 30);
+                e.Graphics.FillRectangle(brownBrush, 145, 320, 30, 155);
+
+                //Draw the square where the brawlers can be placed
+                e.Graphics.DrawRectangle(blackPen, firstTurn);
+                e.Graphics.DrawRectangle(blackPen, secondTurn);
+                e.Graphics.DrawRectangle(blackPen, thirdTurn);
+
+                e.Graphics.DrawRectangle(blackPen, 0, 75, 100, 375);
+
+                e.Graphics.DrawImage(edgarLogo, edgarSquare);
+                e.Graphics.DrawImage(crowLogo, crowSquare);
+                e.Graphics.DrawImage(jackyLogo, jackySquare);
+                e.Graphics.DrawImage(pocoLogo, pocoSquare);
+                e.Graphics.DrawImage(nitaLogo, nitaSquare);
+
+                for (int i = 0; i < edgars.Count; i++)
+                {
+                    e.Graphics.DrawImage(edgarLogo, edgars[i]);
+                }
+            }
+        }
+
+        private void easyButton_MouseHover(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Lime;
+        }
+
+        private void easyButton_MouseLeave(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.White;
+        }
+
+        private void mediumButton_MouseHover(object sender, EventArgs e)
+        {
+            mediumButton.BackColor = Color.Yellow;
+        }
+
+        private void mediumButton_MouseLeave(object sender, EventArgs e)
+        {
+            mediumButton.BackColor = Color.White;
+        }
+
+        private void hardButton_MouseHover(object sender, EventArgs e)
+        {
+            hardButton.BackColor = Color.Orange;
+        }
+
+        private void hardButton_MouseLeave(object sender, EventArgs e)
+        {
+            hardButton.BackColor = Color.White;
+        }
+
+        private void insaneButton_MouseHover(object sender, EventArgs e)
+        {
+            insaneButton.BackColor = Color.Red;
+        }
+
+        private void insaneButton_MouseLeave(object sender, EventArgs e)
+        {
+            insaneButton.BackColor = Color.White;
+        }
+
+        private void exitButton_MouseHover(object sender, EventArgs e)
+        {
+            exitButton.BackColor = Color.Red;
+        }
+
+        private void exitButton_MouseLeave(object sender, EventArgs e)
+        {
+            exitButton.BackColor = Color.White;
+        }
+
+        private void gameTimer_Tick_1(object sender, EventArgs e)
+        {
+            //Check to see if user has grabbed tower
+            
+
+            //check to see if the tower is placed on the arena
+
+            //Check to see if any towers have been upgraded
+
+            //Check to see if any towers have been sold
+
+            //Check to see if it is time to shoot
+
+            //Check to see if there is a bullet on the screen
+
+            //Check to see if there are any bullets off the screen
+
+            //Check to see if the robots have been shot
+
+            //Check to see if the robots have been killed
+
+            //Check to see if any robots made it to the end of the path
+
+            //Are there any lives left
+
+            //Check to see if the wave is completed
+
+            //Check to see if all the waves have been completed
+
+
+
+            Refresh();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void easyButton_Click_1(object sender, EventArgs e)
+        {
+            screen = 2;
+
+            lives = 200;
+            money = 1200;
+            screen = 2;
+        }
+
+        private void mediumButton_Click_1(object sender, EventArgs e)
+        {
+            screen = 2;
+
+            lives = 150;
+            money = 1000;
+            screen = 2;
+        }
+
+        private void hardButton_Click_1(object sender, EventArgs e)
+        {
+            screen = 2;
+
+            lives = 100;
+            money = 800;
+            screen = 2;
+        }
+
+        private void insaneButton_Click_1(object sender, EventArgs e)
+        {
+            screen = 2;
+
+            lives = 1;
+            money = 600;
+            screen = 2;
+        }
+
+        private void dragEdgar_DragOver(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void dragEdgar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                edgaroffset = new Point (e.X, e.Y);
+            }
+        }
+
+        private void dragEdgar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging) 
+            {
+                dragEdgar.Left += e.X - edgaroffset.X;
+                dragEdgar.Top += e.Y - edgaroffset.Y;
+            }
+        }
+
+        private void dragEdgar_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+
+                edgars.Add(new Rectangle(dragEdgar.Left, dragEdgar.Top, 50, 50));
+
+                dragEdgar.Left = 23;
+                dragEdgar.Top = 90;
+
+            }
+        }
+    }
+}
+
